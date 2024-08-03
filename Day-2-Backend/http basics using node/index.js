9830fa442bdebb2a940b733342ae441b9250603e
Anklesh Tumanne <ankleshtumanne77@gmail.com>
2024-08-03 13:36:47 +0530

    todo express

--- Day-2-Backend/http basics using node/index.js
+++ Day-2-Backend/http basics using node/index.js
@@ -7,7 +7,7 @@ app.post("/update_user",(req,res)=>{
     const data=JSON.parse(fs.readFileSync("./db.json","utf-8"))
     const parseData=req.body
     const random_data={"id":2,"email":"anklesh","password":"12345"}
-    if
+    
     console.log("getting data",data)
 })
 app.listen(PORT,(req,res)=>{
